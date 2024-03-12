b1aace12b692ff65ecb810a5498993c57ee72b09
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-12 21:30:22 +0900

    feat: view and create endpoints

--- go.mod
+++ go.mod
@@ -3,3 +3,6 @@ module jade-factory/go-snippetbox
 go 1.22.0
 
 // todo: what is supply-chain attacks?
+// go run .
+// go run main.go
+// go run jade-factory/go-snippetbox
--- main.go
+++ main.go
@@ -8,8 +8,27 @@ import (
 // init home handler
 // "http.ResponseWriter" -> provides methods for assembling a HTTP response and sending it to the user
 // "*http.Request" -> pointer to a struct which holds info about the current request(context?)
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return the handler
+	// would keep executing and also write the "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
@@ -19,10 +38,29 @@ func main() {
 	// register the home func as the handler for the root route
 	// !info Go's servemux treats the URL pattern "/" like a catch-all
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snimppet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
 	// start the server and listen
+	/**
+	In other Go projects or documentation you might sometimes see network addresses written
+	using named ports like ":http" or ":http-alt" instead of a number. If you use a named
+	port then the http.ListenAndServe() function will attempt to look up the relevant port
+	number from your /etc/services file when starting the server, returning an error if a match
+	can’t be found.
+	*/
+
+	/**
+	If you pass nil as the second argument to http.ListenAndServe(), the server will use
+	http.DefaultServeMux for routing.
+
+	for the sake of clarity, maintainability and security, it’s generally a good idea to avoid
+	http.DefaultServeMux and the corresponding helper functions. Use your own locallyscoped
+	servemux instead
+	*/
+
 	err := http.ListenAndServe(":4000", mux)
 
 	// use log.Fatal to log the error and exit.
