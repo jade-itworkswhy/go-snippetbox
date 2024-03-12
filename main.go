1a729441b134cccb9f7542baa46083febccd4d9f
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-12 21:34:20 +0900

    chore: method filter

--- main.go
+++ main.go
@@ -28,6 +28,19 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+
+		// It’s only possible to call w.WriteHeader() once per response, and after the status code
+		// has been written it can’t be changed
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
@@ -39,7 +52,7 @@ func main() {
 	// !info Go's servemux treats the URL pattern "/" like a catch-all
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snimppet/create", snippetCreate)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
