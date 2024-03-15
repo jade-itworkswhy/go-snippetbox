b454ba119de564cb572655aa7710619d40be8acc
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-15 15:30:08 +0900

    feat: routes isolation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -39,24 +39,11 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux() // this makes a function statisfies handler interface
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Register the other application routes as normal.
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	logger.Info("starting server", "addr", *addr)
+	// Call the new app.routes() method to get the servemux containing our routes,
+	// and pass that to http.ListenAndServe().
+	err := http.ListenAndServe(*addr, app.routes())
 
-	err := http.ListenAndServe(*addr, mux)
 	logger.Error(err.Error())
 	os.Exit(1)
 
