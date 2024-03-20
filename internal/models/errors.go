2ab35669fbdebbef9e52cb6b75c78cc3d2d930ff
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-20 17:21:33 +0900

    feat: user sign up

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -5,4 +5,12 @@ import (
 )
 
 // encapsulate the sql error
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	// Add a new ErrInvalidCredentials error. We'll use this later if a user
+	// tries to login with an incorrect email address or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// Add a new ErrDuplicateEmail error. We'll use this later if a user
+	// tries to signup with an email address that's already in use.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
